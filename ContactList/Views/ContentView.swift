7e61e54cb3385b903d269ff1a8b65c36abc5090a
s3915185 <tran.vuhoangsf19@gmail.com>
2023-07-31 17:33:29 +0700

    Initial Commit

---
+++ ContactList/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WelcomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactList()
+    }
+}
